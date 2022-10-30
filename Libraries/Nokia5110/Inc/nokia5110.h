7d34fdcf58bd7090b50deacf19d0a21be93c4b1a
Oğuz Kağan Yağlıoğlu <me@oguzkagan.xyz>
2022-10-30 22:57:31 +0300

    the necessary function to print numbers on the screen added

--- Libraries/Nokia5110/Inc/nokia5110.h
+++ Libraries/Nokia5110/Inc/nokia5110.h
@@ -35,6 +35,20 @@ void drawInRow_buffer(uint8_t* row, const uint8_t* byteMap, size_t start, size_t
 */
 void clearRow_buffer(uint8_t* row, uint32_t start, uint32_t len);
 
+/**
+ * @fn          printNumber_buffer
+ *
+ * @brief       This function writes given number to the screen buffer.
+ *
+ * @param[in]   number number to write in the buffer
+ * @param[in]   screenPosX horizontal position of the number. 0 is most left, 84 is most right
+ * @param[in]   screenPosY number to write in the buffer. 0 is most up 42 (screen size 48 - font size 5) most bottom
+ *
+ * @return      next screenPosX
+ *
+ * @note        this function only writes to the one row, which means if the given position is requires to print half upper row half below row number wont show full 
+ * */
+uint8_t printNumber_buffer(uint32_t number, uint8_t screenPosX, uint8_t screenPosY);
 
 
 #endif /* LIBRARIES_NOKIA5110_INC_NOKIA5110_H_ */
--- Libraries/Nokia5110/Src/nokia5110.c
+++ Libraries/Nokia5110/Src/nokia5110.c
@@ -10,6 +10,32 @@
  */
 uint8_t screenRows[ROW_COUNT][ROW_LEN];
 
+/*
+ * NUMBERS FONT
+ */
+static const uint8_t numbers[10][4] = {
+// '0', 4x5px
+{0x0e, 0x11, 0x11, 0x0e},
+// '1', 3x5px
+{ 0x12, 0x1f, 0x10, 0x0},
+// '2', 4x5px
+{0x12, 0x19, 0x15, 0x12},
+// '3', 4x5px
+{0x0a, 0x11, 0x15, 0x0a},
+// '4', 4x5px
+{0x0c, 0x0a, 0x1f, 0x08},
+// '5', 4x5px
+{0x17, 0x15, 0x15, 0x09},
+// '6', 4x5px
+{0x0e, 0x15, 0x15, 0x08},
+// '7', 4x5px
+{0x01, 0x01, 0x19, 0x06},
+// '8', 4x5px
+{0x0a, 0x15, 0x15, 0x0a},
+// '9', 4x5px
+{0x02, 0x05, 0x15, 0x0e}
+};
+
 void displaySendCommand(SPI_Handle_t* spi, GPIO_Handle_t* dc, GPIO_Handle_t* ce, const uint8_t* buffer, uint32_t len) {
     // Select command mode
     GPIO_WritePin_H(dc, false);
@@ -71,3 +97,22 @@ void drawInRow_buffer(uint8_t* row, const uint8_t* byteMap, size_t start, size_t
     for (i = 0; i < len && (start + i) < ROW_LEN; i++) row[start + i] |= byteMap[i];
 }
 
+
+uint8_t printNumber_buffer(uint32_t number, uint8_t screenPosX, uint8_t screenPosY) {
+    if (number >= 10) screenPosX = printNumber_buffer(number / 10, screenPosX, screenPosY);
+    number %= 10;
+    uint8_t i = 0, s = 4;
+    if (number == 1) s = 3;
+    uint16_t tmpFont;
+    for (i = 0; i < s; ++i) {
+        if (screenPosX >= ROW_LEN) break; // stop drawing if it overflows the screen
+        tmpFont = ((uint16_t)numbers[number][i]) << (screenPosY % 8);
+
+        screenRows[screenPosY / 8][screenPosX] |= (uint8_t)((tmpFont) & 0xFF);
+        if ((screenPosY / 8) >= 1)
+            screenRows[(screenPosY / 8) + 1][screenPosX] |= (uint8_t)((tmpFont >> 8) & 0xFF);
+        screenPosX++;
+    }
+    screenPosX += 1; // space for the next character
+    return screenPosX;
+}
